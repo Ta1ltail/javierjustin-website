dd005028adb125cee5bda1d6f84bd18489f6cf79
Justin Javier <jjavier292002@gmail.com>
2026-05-10 19:05:58 +0800

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">
-          Justin Javier Pogis
+          Justin Javier
         </h1>
 
         <p className="text-zinc-400 text-lg">
