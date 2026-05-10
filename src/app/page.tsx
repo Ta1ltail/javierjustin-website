c6e9dc420ea0472322da35a4a2e5b96b9020c528
Justin Javier <jjavier292002@gmail.com>
2026-05-10 18:31:59 +0800

    Initial Personal Website Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold mb-4">
+          Justin Javier
+        </h1>
+
+        <p className="text-zinc-400 text-lg">
+          Fullstack Developer
+        </p>
+      </div>
+    </main>
+  );
+}
